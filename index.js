13c3c4bf369c4bc1667f3352aa77453277b4967e
Nirmal Jose <nirmaljosehere@gmail.com>
2025-01-23 15:39:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 
   try {
     const response = await fetch(
-      `https://my.api.mockaroo.com/qatar_airways_api.json?key=1b870620&__method=POST&flightID=${flightID}`,
+      `https://my.api.mockaroo.com/qatar_airways_api.json?key=1b870620&flightID=${flightID}`,
       { method: 'GET' }
     );
 
