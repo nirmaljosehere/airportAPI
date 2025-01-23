ceb37f3bef64ec2f7cb31c55448f74155509d53a
Nirmal Jose <nirmaljosehere@gmail.com>
2025-01-23 15:36:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
   try {
     const response = await fetch(
       `https://my.api.mockaroo.com/qatar_airways_api.json?key=1b870620&__method=POST&flightID=${flightID}`,
-      { method: 'POST' }
+      { method: 'GET' }
     );
 
     if (!response.ok) throw new Error("Failed to fetch data");
